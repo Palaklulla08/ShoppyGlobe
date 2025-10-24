b1ac1b96e4103352a7dc6bd59fa9dbc3cd13795b
palaklulla_08 <palaklulla54@gmail.com>
2025-10-24 13:17:39 +0530

    loader utils

---
+++ shoppyglobe/src/utils/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Loader component
+ * - Simple reusable component to show a loading indicator
+ * - Can be used anywhere data is being fetched
+ */
+export default function Loader() {
+  return (
+    <div className="center">
+      Loading…
+    </div>
+  );
+}
